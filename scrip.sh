898dcc3dec45bc83bdc58d8c11b74f3a34257752
Jackson Schreiber <jacksonschreiber@hotmail.com>
2020-07-30 22:49:14 -0400

    implement associative array

--- scrip.sh
+++ scrip.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+
+
+
+aaAdd () {
+#$1 = service   $2 = host(ip)
+    i=0
+    while true; do
+        if [ ${aa[$1,$i]} ]; then
+            i=$(( $i+1 ))
+        else
+            aa[$1,$i]=$2
+            break
+        fi
+    done
+
+}
+
 declare -A aa
 
 i=4
@@ -11,8 +28,10 @@ while true; do
     ip=$(echo $host | grep -Po '\d+\.\d+\.\d+.\d+')
     
     services=$(echo "$host" | grep -Po '(?<=open\s{2})([\w-]+)$')
-    echo "$services" >> services
-
+    
+    for x in $services; do
+        aaAdd $x $ip
+    done
 
 
   elif [[ $(echo "$host" | wc -w ) -eq 0 ]]; then
@@ -21,3 +40,13 @@ while true; do
 
   i=$(( $i+1 )) 
 done
+
+#echo "${!aa[@]}"
+
+for x in ${!aa[@]}; do
+    echo "$x" >> output
+done
+#cat output | sort -d
+
+
+
