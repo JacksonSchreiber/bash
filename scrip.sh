9016c163d1f48eb909e3e820e1a4d122c88fb1c8
Jackson Schreiber <jacksonschreiber@hotmail.com>
2020-08-03 22:24:37 +0000

    Final revision. takes path as argument

--- scrip.sh
+++ scrip.sh
@@ -1,52 +1,41 @@
 #!/bin/bash
 
+declare -A aa #declare associative array (hash table)
 
-
-
-aaAdd () {
-#$1 = service   $2 = host(ip)
-    i=0
-    while true; do
-        if [ ${aa[$1,$i]} ]; then
-            i=$(( $i+1 ))
-        else
-            aa[$1,$i]=$2
-            break
-        fi
-    done
-
-}
-
-declare -A aa
-
-i=4
+i=4 # start on line 4
 
 while true; do
-  host=$(awk NR==$i RS="\n\n" 'NMAP_all_hosts.txt')
-  if [[  $(echo "$host" | wc -l) -gt 5 ]]; then
+  host=$(awk NR==$i RS="\n\n" "$1") #split up by individual host 
+  if [[  $(echo "$host" | wc -l) -gt 5 ]]; then #ignore hosts with less than 5 lines of info
 
-    ip=$(echo $host | grep -Po '\d+\.\d+\.\d+.\d+')
+    ip=$(echo $host | grep -Po '\d+\.\d+\.\d+.\d+') #filter ip
     
-    services=$(echo "$host" | grep -Po '(?<=open\s{2})([\w-]+)$')
+    services=$(echo "$host" | grep -Po '(?<=open\s{2})([\w-]+)$') #filter service
     
-    for x in $services; do
-        aaAdd $x $ip
+    for x in $services; do #add all host's services as keys, set host's IP as their value
+      aa[$x]+="$ip
+"
     done
 
 
-  elif [[ $(echo "$host" | wc -w ) -eq 0 ]]; then
+  elif [[ $(echo "$host" | wc -w ) -eq 0 ]]; then #at end of file, break loop
     break
   fi
 
   i=$(( $i+1 )) 
 done
 
-#echo "${!aa[@]}"
 
-for x in ${!aa[@]}; do
-    echo "$x" >> output
+for x in ${!aa[@]}; do #display services and their associated hosts. only unique hosts
+  echo "Service: $x Count: $(( $(echo "${aa[$x]}" | uniq | wc -l) - 1 ))
+=======================================" #get count of IPs per service
+  echo "${aa[$x]}" | uniq
 done
-#cat output | sort -d
+
+
+
+
+
 
 
 
